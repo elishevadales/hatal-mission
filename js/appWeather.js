1d3688a494576742221ee3700b5ffd0dbb17405b
Mona Lisa <elisheva1770@gmail.com>
2023-11-12 22:25:43 +0200

    one card

---
+++ js/appWeather.js
@@ -0,0 +1,22 @@
+const countriesAr = [
+    {
+        name: "eilat",
+        lon: 34.9455,
+        lat: 29.5582
+    },
+    {
+        name: "london",
+        lon: -0.0932,
+        let: 51.5162
+    },
+    {
+        name: "new-york",
+        lon: -73.983,
+        let: 40.6907
+    },
+    {
+        name: "alaska",
+        lon: -153.8572,
+        let: 66.0205
+    }
+]
