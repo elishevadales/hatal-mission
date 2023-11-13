3e5d90cac73135f4ab82eb14a439f8317671ea75
Mona Lisa <elisheva1770@gmail.com>
2023-11-13 23:05:43 +0200

    icons

--- js/classCard.js
+++ js/classCard.js
@@ -22,7 +22,12 @@ class Card {
             <p class="text-info">${this.weather}</p>
         </div>
         <div class="icon">
-        <img src="http://openweathermap.org/img/wn/${this.icon}.png" alt="Weather Icon">
+        
+        ${this.temp <= 20? `<i class="fa-regular fa-snowflake" style="color: #719dea; font-size: 40px;"></i>` : ""}
+        ${this.temp < 30 && this.temp >20? `<i class="fa-solid fa-cloud" style="color: #78e1e8; font-size: 40px;"></i>` : ""}
+        ${this.temp >= 30 ? `<i class="fa-solid fa-sun" style="color: #f5e10a;  font-size: 40px;"></i>` : ""}
+           
+         <img src="http://openweathermap.org/img/wn/${this.icon}.png" alt="Weather Icon" style="display:none">
         <weather number="501" value="moderate rain" icon="10d"/>
         </div>
 
@@ -30,11 +35,11 @@ class Card {
     <div class="bottom d-flex justify-content-between">
         <div class="temp1 text-center">
             <p>טמפ' נמדדת</p>
-            <h2 style="direction: ltr">${this.temp}</h2>
+            <h2 style="direction: ltr">${this.temp}C°</h2>
         </div>
         <div class="temp2 text-center">
             <p>טמפ' מורגשת</p>
-            <h2 style="direction: ltr">${this.feels_like}</h2>
+            <h2 style="direction: ltr">${this.feels_like}C°</h2>
         </div>
         <div class="temp1 text-center">
             <p>לחות</p>
